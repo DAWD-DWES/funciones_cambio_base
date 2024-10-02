ba746fd5b4d1fd2f8599227504c4461e81e98d78
jivanrod <jivanrod@gmail.com>
2024-10-02 19:59:13 +0200

    update

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include "funciones_cambio_base.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Conversión de números</title>
+    </head>
+    <body>
+        <h1><?= "El número 123 en base 16 es equivalente al número " . x2y(123, 16, 2) . " en base 2" ?></h1>
+        <h1><?= "El número 32 en base 5 es equivalente al número " . x2y(32, 5, 10) . " en base 10" ?></h1>
+    </body>
+</html>
+
